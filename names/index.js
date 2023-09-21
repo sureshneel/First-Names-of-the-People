b019181ebc2853606b1f129a26007cc7452f0301
sureshneel <sureshmadigundu123@gmail.com>
2023-09-21 17:32:29 +0000

    First Names of the People

---
+++ names/index.js
@@ -0,0 +1,8 @@
+const peopleNames = require("../country/state/city/index");
+const getFirstName = require("../utilities/utils/index");
+const getPeopleInCity = (peopleNames) => {
+  return getFirstName(peopleNames);
+};
+
+module.exports = getPeopleInCity;
+console.log(getPeopleInCity(peopleNames));
